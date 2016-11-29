deac6eb5b2bf209ce5a5c8b4a3437de1e72474e1
mumeiyamibito <mumeiyamibito@gmail.com>
2016-11-29 15:53:49 +0900

    add: PIEDA 出力機能を追加した

--- cpf2csv.py
+++ cpf2csv.py
@@ -19,8 +19,9 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description = "cpf2csv - convert log for ABINIT-MP to CSV", formatter_class=argparse.RawTextHelpFormatter)
 	parser.add_argument("-i", dest = "input", metavar = "LOG", required = True, help = "LOG for ABINIT-MP")
 	parser.add_argument("-o", dest = "prefix", help = "prefix for output")
-	parser.add_argument("-O", dest = "flag_overwrite", action = "store_true", default = False, help = "overwrite forcibly")
-	parser.add_argument("-N", dest = "cancel", action = "store_true", default = False, help = "do NOT cancel the interaction between adjacent fragments")
+	parser.add_argument("-O", dest = "flag_overwrite", action = "store_true", default = False, help = "overwrite forcibly (Default: False)")
+	parser.add_argument("-p", dest = "flag_pieda", action = "store_true", default = False, help = "output with PIEDA results (Default: False)")
+	parser.add_argument("-N", dest = "flag_cancel", action = "store_true", default = False, help = "do NOT cancel the interaction between adjacent fragments (default: False)")
 	args = parser.parse_args()
 
 	basic.check_exist(args.input, 2)
@@ -30,13 +31,19 @@ if __name__ == '__main__':
 	energy_HFs = []
 	energy_MP2s = []
 	energy_tots = []
+	energy_ESs = []
+	energy_EXs = []
+	energy_mixs = []
+	energy_DIs = []
+	energy_qs = []
 
 	with open(args.input, "r") as obj_input:
 		re_wsp = re.compile(r"[\s\t]+")
 		re_fragmentation = re.compile(r"Frag\.   Elec\.   ATOM")
 		re_bonded_atom = re.compile(r"Bonded Atom")
-		re_IFIE1 = re.compile(r"## ((HF)|(MP2))-IFIE")
-		re_IFIE2 = re.compile(r"-{5,}")
+		re_IFIE = re.compile(r"## ((HF)|(MP2))-IFIE")
+		re_separator = re.compile(r"-{5,}")
+		re_PIEDA = re.compile(r"## PIEDA")
 		re_empty = re.compile(r"^[\s\t]*\n$")
 
 		flag_read = 0
@@ -46,7 +53,7 @@ if __name__ == '__main__':
 			elif re_bonded_atom.search(line):
 				flag_read = 2
 
-			elif re_IFIE1.search(line):
+			elif re_IFIE.search(line):
 				flag_read = 3
 
 				# IFIE 格納用配列を初期化
@@ -80,10 +87,58 @@ if __name__ == '__main__':
 							energy_MP2s[i].append(i)
 							energy_tots[i].append(i)
 
-			elif flag_read == 3 and re_IFIE2.search(line):
-				flag_read = 4
+			elif flag_read == 3:
+				if re_separator.search(line):
+					flag_read = 4
 
-			elif re_empty.search(line) and flag_read != 3:
+			elif flag_read == 5:
+				if re_separator.search(line):
+					flag_read = 6
+					# `PIEDA 格納用配列を初期化
+					for i in range(len(fragment_atoms) + 1):
+						if i != 0:
+							#  通常の行
+							energy_ESs.append([])
+							energy_EXs.append([])
+							energy_mixs.append([])
+							energy_DIs.append([])
+							energy_qs.append([])
+						else:
+							# 最初の行はラベル
+							labels = list(range(1, len(fragment_atoms) + 1))
+							labels.insert(0, "")
+							energy_ESs.insert(0, labels)
+							energy_EXs.insert(0, labels)
+							energy_mixs.insert(0, labels)
+							energy_DIs.insert(0, labels)
+							energy_qs.insert(0, labels)
+							continue
+						for j in range(len(fragment_atoms) + 1):
+							# 通常行の中身
+							if i == j:
+								energy_ESs[i].append(0.0)
+								energy_EXs[i].append(0.0)
+								energy_mixs[i].append(0.0)
+								energy_DIs[i].append(0.0)
+								energy_qs[i].append(0.0)
+							elif j != 0:
+								energy_ESs[i].append("-")
+								energy_EXs[i].append("-")
+								energy_mixs[i].append("-")
+								energy_DIs[i].append("-")
+								energy_qs[i].append("-")
+							else:
+								# 最初の列はラベル
+								energy_ESs[i].append(i)
+								energy_EXs[i].append(i)
+								energy_mixs[i].append(i)
+								energy_DIs[i].append(i)
+								energy_qs[i].append(i)
+
+			elif args.flag_pieda == True and re_PIEDA.search(line):
+				flag_read = 5
+
+			elif re_empty.search(line):
 				flag_read = 0
 
 			elif flag_read == 1:
@@ -114,15 +169,15 @@ if __name__ == '__main__':
 			elif flag_read == 4:
 				# IFIE 取得
 				frag_i = int(line[8:13].strip())
-				frag_j = int(line[13:21].strip())
+				frag_j = int(line[13:18].strip())
 				energy_HF = float(line[39:50].strip()) * 627.5095
 				energy_MP2 = float(line[50:61].strip()) * 627.5095
 
-				if args.cancel == False:
+				if args.flag_cancel == False:
 					if [frag_i, frag_j] in fragment_connects or [frag_j, frag_i] in fragment_connects:
 						# フラグメントが共有結合している場合
-						energy_HF = 0
-						energy_MP2 = 0
+						energy_HF = 0.0
+						energy_MP2 = 0.0
 
 				energy_HFs[frag_i][frag_j] = energy_HF
 				energy_HFs[frag_j][frag_i] = energy_HF
@@ -130,6 +185,36 @@ if __name__ == '__main__':
 				energy_MP2s[frag_j][frag_i] = energy_MP2
 				energy_tots[frag_i][frag_j] = energy_HF + energy_MP2
 				energy_tots[frag_j][frag_i] = energy_HF + energy_MP2
+
+			elif flag_read == 6:
+				# PIEDA 取得
+				frag_i = int(line[8:13].strip())
+				frag_j = int(line[13:18].strip())
+				energy_ES = float(line[18:33].strip())
+				energy_EX = float(line[33:48].strip())
+				energy_mix = float(line[48:63].strip())
+				energy_DI = float(line[63:78].strip())
+				energy_q = float(line[78:93].strip())
+
+				if args.flag_cancel == False:
+					if [frag_i, frag_j] in fragment_connects or [frag_j, frag_i] in fragment_connects:
+						# フラグメントが共有結合している場合
+						energy_ES = 0.0
+						energy_EX = 0.0
+						energy_mix = 0.0
+						energy_DI = 0.0
+						energy_q = 0.0
+
+				energy_ESs[frag_i][frag_j] = energy_ES
+				energy_ESs[frag_j][frag_i] = energy_ES
+				energy_EXs[frag_i][frag_j] = energy_EX
+				energy_EXs[frag_j][frag_i] = energy_EX
+				energy_mixs[frag_i][frag_j] = energy_mix
+				energy_mixs[frag_j][frag_i] = energy_mix
+				energy_DIs[frag_i][frag_j] = energy_DI
+				energy_DIs[frag_j][frag_i] = energy_DI
+				energy_qs[frag_i][frag_j] = energy_q
+				energy_qs[frag_j][frag_i] = energy_q
 
 	# 出力ファイル
 	prefix = ""
@@ -146,6 +231,7 @@ if __name__ == '__main__':
 	with open(output, "w") as obj_output:
 		csv_writer = csv.writer(obj_output, lineterminator = "\n")
 		csv_writer.writerows(energy_HFs)
+		sys.stderr.write("%s was created\n" % output)
 
 	output = prefix + "_MP2.csv"
 	if args.flag_overwrite == False:
@@ -153,6 +239,7 @@ if __name__ == '__main__':
 	with open(output, "w") as obj_output:
 		csv_writer = csv.writer(obj_output, lineterminator = "\n")
 		csv_writer.writerows(energy_MP2s)
+		sys.stderr.write("%s was created\n" % output)
 
 	output = prefix + "_tot.csv"
 	if args.flag_overwrite == False:
@@ -160,3 +247,46 @@ if __name__ == '__main__':
 	with open(output, "w") as obj_output:
 		csv_writer = csv.writer(obj_output, lineterminator = "\n")
 		csv_writer.writerows(energy_tots)
+		sys.stderr.write("%s was created\n" % output)
+
+	if args.flag_pieda == True:
+		# PIEDA の結果出力
+		output = prefix + "_ES.csv"
+		if args.flag_overwrite == False:
+			basic.check_overwrite(output)
+		with open(output, "w") as obj_output:
+			csv_writer = csv.writer(obj_output, lineterminator = "\n")
+			csv_writer.writerows(energy_ESs)
+			sys.stderr.write("%s was created\n" % output)
+
+		output = prefix + "_EX.csv"
+		if args.flag_overwrite == False:
+			basic.check_overwrite(output)
+		with open(output, "w") as obj_output:
+			csv_writer = csv.writer(obj_output, lineterminator = "\n")
+			csv_writer.writerows(energy_EXs)
+			sys.stderr.write("%s was created\n" % output)
+
+		output = prefix + "_MIX.csv"
+		if args.flag_overwrite == False:
+			basic.check_overwrite(output)
+		with open(output, "w") as obj_output:
+			csv_writer = csv.writer(obj_output, lineterminator = "\n")
+			csv_writer.writerows(energy_mixs)
+			sys.stderr.write("%s was created\n" % output)
+
+		output = prefix + "_DI.csv"
+		if args.flag_overwrite == False:
+			basic.check_overwrite(output)
+		with open(output, "w") as obj_output:
+			csv_writer = csv.writer(obj_output, lineterminator = "\n")
+			csv_writer.writerows(energy_DIs)
+			sys.stderr.write("%s was created\n" % output)
+
+		output = prefix + "_qtrans.csv"
+		if args.flag_overwrite == False:
+			basic.check_overwrite(output)
+		with open(output, "w") as obj_output:
+			csv_writer = csv.writer(obj_output, lineterminator = "\n")
+			csv_writer.writerows(energy_qs)
+			sys.stderr.write("%s was created\n" % output)
