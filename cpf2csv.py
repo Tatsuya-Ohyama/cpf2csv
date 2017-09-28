7e2d39287a2a46804eed46ad0a00d180ed9340d1
Tatsuya Ohyama <t.ohyama.0109.02442@gmail.com>
2017-09-28 16:25:40 +0900

    replace loading and analyzing log file to EnergyData class

--- cpf2csv.py
+++ cpf2csv.py
@@ -10,6 +10,7 @@ signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 import argparse
 from py_module_basic import basic
+from mods import EnergyData
 
 # =============== functions =============== #
 # 二次リストから特定の値を検索し、index を返す
@@ -72,206 +73,8 @@ if __name__ == '__main__':
 	if flag_ES or flag_EX or flag_CT or flag_DI or flag_q:
 		flag_pieda = True
 
-	fragment_atoms = []
-	fragment_connects = []
-	energy_HFs = []
-	energy_MP2s = []
-	energy_tots = []
-	energy_ESs = []
-	energy_EXs = []
-	energy_CTs = []
-	energy_DIs = []
-	energy_qs = []
-	atomic_charges = [["Fragment No.", "Atom No.", "Atom Name", "Atomic pop.", "Net charge"]]
-
-	with open(args.input, "r") as obj_input:
-		re_wsp = re.compile(r"[\s\t]+")
-		re_fragmentation = re.compile(r"Frag\.   Elec\.   ATOM")
-		re_bonded_atom = re.compile(r"Bonded Atom")
-		re_IFIE = re.compile(r"## ((HF)|(MP2))-IFIE")
-		re_separator = re.compile(r"-{5,}")
-		re_PIEDA = re.compile(r"## PIEDA")
-		re_charge = re.compile(r"No\. Atom   Atomic pop\.  Net charge")
-		re_empty = re.compile(r"^[\s\t]*\n$")
-
-		flag_read = 0
-		for line in obj_input:
-			if re_fragmentation.search(line):
-				flag_read = 1
-			elif re_bonded_atom.search(line):
-				flag_read = 2
-
-			elif re_IFIE.search(line):
-				flag_read = 3
-
-				# IFIE 格納用配列を初期化
-				for i in range(len(fragment_atoms) + 1):
-					if i != 0:
-						#  通常の行
-						energy_HFs.append([])
-						energy_MP2s.append([])
-						energy_tots.append([])
-					else:
-						# 最初の行はラベル
-						labels = list(range(1, len(fragment_atoms) + 1))
-						labels.insert(0, "")
-						energy_HFs.insert(0, labels)
-						energy_MP2s.insert(0, labels)
-						energy_tots.insert(0, labels)
-						continue
-					for j in range(len(fragment_atoms) + 1):
-						# 通常行の中身
-						if i == j:
-							energy_HFs[i].append(0.0)
-							energy_MP2s[i].append(0.0)
-							energy_tots[i].append(0.0)
-						elif j != 0:
-							energy_HFs[i].append("-")
-							energy_MP2s[i].append("-")
-							energy_tots[i].append("-")
-						else:
-							# 最初の列はラベル
-							energy_HFs[i].append(i)
-							energy_MP2s[i].append(i)
-							energy_tots[i].append(i)
-
-			elif flag_read == 3:
-				if re_separator.search(line):
-					flag_read = 4
-
-			elif flag_read == 5:
-				if re_separator.search(line):
-					flag_read = 6
-					# `PIEDA 格納用配列を初期化
-					for i in range(len(fragment_atoms) + 1):
-						if i != 0:
-							#  通常の行
-							energy_ESs.append([])
-							energy_EXs.append([])
-							energy_CTs.append([])
-							energy_DIs.append([])
-							energy_qs.append([])
-						else:
-							# 最初の行はラベル
-							labels = list(range(1, len(fragment_atoms) + 1))
-							labels.insert(0, "")
-							energy_ESs.insert(0, labels)
-							energy_EXs.insert(0, labels)
-							energy_CTs.insert(0, labels)
-							energy_DIs.insert(0, labels)
-							energy_qs.insert(0, labels)
-							continue
-						for j in range(len(fragment_atoms) + 1):
-							# 通常行の中身
-							if i == j:
-								energy_ESs[i].append(0.0)
-								energy_EXs[i].append(0.0)
-								energy_CTs[i].append(0.0)
-								energy_DIs[i].append(0.0)
-								energy_qs[i].append(0.0)
-							elif j != 0:
-								energy_ESs[i].append("-")
-								energy_EXs[i].append("-")
-								energy_CTs[i].append("-")
-								energy_DIs[i].append("-")
-								energy_qs[i].append("-")
-							else:
-								# 最初の列はラベル
-								energy_ESs[i].append(i)
-								energy_EXs[i].append(i)
-								energy_CTs[i].append(i)
-								energy_DIs[i].append(i)
-								energy_qs[i].append(i)
-
-			elif flag_pieda and re_PIEDA.search(line):
-				flag_read = 5
-
-			elif re_charge.search(line):
-				flag_read = 7
-
-			elif re_empty.search(line):
-				flag_read = 0
-
-			elif flag_read == 1:
-				# フラグメント構成原子取得
-				fragment_num = line[5:13].strip()
-				atoms = list(map(lambda x : int(x), re_wsp.split(line[23:].strip())))
-				if len(fragment_num) == 0:
-					# 継続の場合
-					fragment_atoms[len(fragment_atoms) - 1].extend(atoms)
-
-				else:
-					# 新たなフラグメントの場合
-					fragment_atoms.append(atoms)
-
-			elif flag_read == 2:
-				# フラグメント接続情報取得
-				datas = list(map(lambda x : int(x), re_wsp.split(line.strip())))
-				tmp_connects = []
-				for atom in datas[1:3]:
-					index = 0
-					for info in fragment_atoms:
-						index += 1
-						if atom in info:
-							tmp_connects.append(index)
-							break
-				fragment_connects.append(tmp_connects)
-
-			elif flag_read == 4:
-				# IFIE 取得
-				frag_i = int(line[8:13].strip())
-				frag_j = int(line[13:18].strip())
-				energy_HF = float(line[39:50].strip()) * 627.5095
-				energy_MP2 = float(line[50:61].strip()) * 627.5095
-
-				if args.flag_cancel == False:
-					if [frag_i, frag_j] in fragment_connects or [frag_j, frag_i] in fragment_connects:
-						# フラグメントが共有結合している場合
-						energy_HF = 0.0
-						energy_MP2 = 0.0
-
-				energy_HFs[frag_i][frag_j] = energy_HF
-				energy_HFs[frag_j][frag_i] = energy_HF
-				energy_MP2s[frag_i][frag_j] = energy_MP2
-				energy_MP2s[frag_j][frag_i] = energy_MP2
-				energy_tots[frag_i][frag_j] = energy_HF + energy_MP2
-				energy_tots[frag_j][frag_i] = energy_HF + energy_MP2
-
-			elif flag_read == 6:
-				# PIEDA 取得
-				frag_i = int(line[8:13].strip())
-				frag_j = int(line[13:18].strip())
-				energy_ES = float(line[18:33].strip())
-				energy_EX = float(line[33:48].strip())
-				energy_CT = float(line[48:63].strip())
-				energy_DI = float(line[63:78].strip())
-				energy_q = float(line[78:93].strip())
-
-				if args.flag_cancel == False:
-					if [frag_i, frag_j] in fragment_connects or [frag_j, frag_i] in fragment_connects:
-						# フラグメントが共有結合している場合
-						energy_ES = 0.0
-						energy_EX = 0.0
-						energy_CT = 0.0
-						energy_DI = 0.0
-						energy_q = 0.0
-
-				energy_ESs[frag_i][frag_j] = energy_ES
-				energy_ESs[frag_j][frag_i] = energy_ES
-				energy_EXs[frag_i][frag_j] = energy_EX
-				energy_EXs[frag_j][frag_i] = energy_EX
-				energy_CTs[frag_i][frag_j] = energy_CT
-				energy_CTs[frag_j][frag_i] = energy_CT
-				energy_DIs[frag_i][frag_j] = energy_DI
-				energy_DIs[frag_j][frag_i] = energy_DI
-				energy_qs[frag_i][frag_j] = energy_q
-				energy_qs[frag_j][frag_i] = -1 * energy_q
-
-			elif flag_read == 7:
-				line = line.strip()
-				datas = re_wsp.split(line)
-				datas.insert(0, search_list(int(datas[0]), fragment_atoms, 1))
-				atomic_charges.append(datas)
+	# データ読み込み＆解析
+	energy = EnergyData.EnergyData(args.input)
 
 
 	# 出力ファイル
@@ -282,14 +85,13 @@ if __name__ == '__main__':
 		prefix = re.sub(r"\..{3,4}$", "", args.input)
 
 	import csv
-
 	if flag_total:
 		output = prefix + "_tot.csv"
 		if args.flag_overwrite == False:
 			basic.check_overwrite(output)
 		with open(output, "w") as obj_output:
 			csv_writer = csv.writer(obj_output, lineterminator = "\n")
-			csv_writer.writerows(energy_tots)
+			csv_writer.writerows(energy.output_energy("Total"))
 			sys.stderr.write("create: %s (total)\n" % output)
 
 	if flag_HF:
@@ -298,7 +100,7 @@ if __name__ == '__main__':
 			basic.check_overwrite(output)
 		with open(output, "w") as obj_output:
 			csv_writer = csv.writer(obj_output, lineterminator = "\n")
-			csv_writer.writerows(energy_HFs)
+			csv_writer.writerows(energy.output_energy("HF"))
 			sys.stderr.write("create: %s (hartree)\n" % output)
 
 	if flag_corr:
@@ -307,7 +109,7 @@ if __name__ == '__main__':
 			basic.check_overwrite(output)
 		with open(output, "w") as obj_output:
 			csv_writer = csv.writer(obj_output, lineterminator = "\n")
-			csv_writer.writerows(energy_MP2s)
+			csv_writer.writerows(energy.output_energy("Total"))
 			sys.stderr.write("create: %s (correlation)\n" % output)
 
 	if flag_ES:
@@ -316,7 +118,7 @@ if __name__ == '__main__':
 			basic.check_overwrite(output)
 		with open(output, "w") as obj_output:
 			csv_writer = csv.writer(obj_output, lineterminator = "\n")
-			csv_writer.writerows(energy_ESs)
+			csv_writer.writerows(energy.output_energy("ES"))
 			sys.stderr.write("create: %s (electrostatic)\n" % output)
 
 	if flag_EX:
@@ -325,7 +127,7 @@ if __name__ == '__main__':
 			basic.check_overwrite(output)
 		with open(output, "w") as obj_output:
 			csv_writer = csv.writer(obj_output, lineterminator = "\n")
-			csv_writer.writerows(energy_EXs)
+			csv_writer.writerows(energy.output_energy("EX"))
 			sys.stderr.write("create: %s (exchange)\n" % output)
 
 	if flag_CT:
@@ -334,7 +136,7 @@ if __name__ == '__main__':
 			basic.check_overwrite(output)
 		with open(output, "w") as obj_output:
 			csv_writer = csv.writer(obj_output, lineterminator = "\n")
-			csv_writer.writerows(energy_CTs)
+			csv_writer.writerows(energy.output_energy("CT"))
 			sys.stderr.write("create: %s (chargetransfer-mix)\n" % output)
 
 	if flag_DI:
@@ -343,7 +145,7 @@ if __name__ == '__main__':
 			basic.check_overwrite(output)
 		with open(output, "w") as obj_output:
 			csv_writer = csv.writer(obj_output, lineterminator = "\n")
-			csv_writer.writerows(energy_DIs)
+			csv_writer.writerows(energy.output_energy("DI"))
 			sys.stderr.write("create: %s (dispersion)\n" % output)
 
 	if flag_q:
@@ -352,7 +154,7 @@ if __name__ == '__main__':
 			basic.check_overwrite(output)
 		with open(output, "w") as obj_output:
 			csv_writer = csv.writer(obj_output, lineterminator = "\n")
-			csv_writer.writerows(energy_qs)
+			csv_writer.writerows(energy.output_energy("Q"))
 			sys.stderr.write("create: %s (chargetransfer-amount)\n" % output)
 
 	if args.flag_ac:
