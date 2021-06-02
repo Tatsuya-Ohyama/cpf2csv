388f93449ccc6d7f869d19d20c93e6b24addcfdf
Tatsuya Ohyama <t-ohyama@konan-u.ac.jp>
2021-06-02 17:50:04 +0900

    style: change style based on PEP8

--- mods/EnergyData.py
+++ mods/EnergyData.py
@@ -8,10 +8,14 @@ EnergyData class
 import sys, re, copy
 import numpy as np
 
+
+
 # =============== const =============== #
-au = 627.5095
-digit = 4
-re_atomic_charge = re.compile(r"\d+[\s\t]+\D+(:?[\s\t]+-?\d+\.\d+){2}")
+AU = 627.5095
+DIGIT = 4
+RE_ATOMIC_CHARGE = re.compile(r"\d+[\s\t]+\D+(:?[\s\t]+-?\d+\.\d+){2}")
+RE_IFIE = re.compile(r"## ((HF)|(MP2))-IFIE")
+
 
 
 # =============== classes =============== #
@@ -33,41 +37,45 @@ class EnergyData:
 
 		self._load_file(input_file)
 
+
 	def _load_file(self, input_file):
-		""" ファイルを読み込むメソッド """
-		re_fragmentation = re.compile(r"Frag\.   Elec\.   ATOM")
-		re_IFIE = re.compile(r"## ((HF)|(MP2))-IFIE")
-		re_PIEDA = re.compile(r"## PIEDA")
-		re_separator = re.compile(r"-{5,}")
-		re_charge = re.compile(r"No\. Atom   Atomic pop\.  Net charge")
+		"""
+		ファイルを読み込むメソッド
+
+		Args:
+			input_file (str): ABINIT-MP の .out および .log ファイル
+
+		Returns:
+			self
+		"""
 		distance_idx = []
 
 		flag_read = [0,0]
 		with open(input_file, "r") as obj_input:
-			for line_idx, line in enumerate(obj_input):
-				if "ERROR" in line:
+			for line_idx, line_val in enumerate(obj_input):
+				if "ERROR" in line_val:
 					sys.stderr.write("ERROR: ERROR in .log at {0}.\n".format(line_idx + 1))
 					sys.exit(1)
 
 				if flag_read[0] == 0:
 					# フラグ分類
-					if re_fragmentation.search(line):
+					if "Frag.   Elec.   ATOM" in line_val:
 						flag_read[0] = 1
-					elif re_IFIE.search(line):
+					elif RE_IFIE.search(line_val):
 						flag_read[0] = 2
-					elif re_PIEDA.search(line):
+					elif "## PIEDA" in line_val:
 						flag_read[0] = 3
-					elif re_charge.search(line):
+					elif "No. Atom   Atomic pop.  Net charge" in line_val:
 						flag_read[0] = 4
 
 				elif flag_read[0] == 1:
 					# フラグメント構成原子の取得
-					if len(line.strip()) == 0:
+					if len(line_val.strip()) == 0:
 						flag_read = [0,0]
 						continue
 
-					label = line[5:13].strip()
-					atoms = [int(x) for x in line[23:].strip().split()]
+					label = line_val[5:13].strip()
+					atoms = [int(x) for x in line_val[23:].strip().split()]
 					if label:
 						self._label.append(int(label))
 						self._frag_atom.append(atoms)
@@ -83,22 +91,22 @@ class EnergyData:
 						self._charge_frag = [0.0 for i in range(len(self._frag_atom))]
 						flag_read[1] = 1
 
-					elif re_separator.search(line):
+					elif "------" in line_val:
 						flag_read[1] = 2
 
 					elif flag_read[1] == 2:
-						if len(line.strip()) == 0:
+						if len(line_val.strip()) == 0:
 							flag_read = [0,0]
 							continue
 
-						i = int(line[8:13].strip()) - 1
-						j = int(line[13:18].strip()) - 1
-						distance = float(line[18:30].strip())
+						i = int(line_val[8:13].strip()) - 1
+						j = int(line_val[13:18].strip()) - 1
+						distance = float(line_val[18:30].strip())
 
-						energies = [float(x.strip()) for x in [line[39:50], line[50:61]]]
+						energies = [float(x.strip()) for x in [line_val[39:50], line_val[50:61]]]
 
 						if distance == 0.000000:
-							distance_idx.append(line[8:18].strip())
+							distance_idx.append(line_val[8:18].strip())
 							energies = [0.0 for x in energies]
 
 						self._energy_HF[i][j] = self._energy_HF[j][i] = energies[0]
@@ -116,78 +124,131 @@ class EnergyData:
 						self._charge_frag = [0.0 for i in range(len(self._frag_atom))]
 						flag_read[1] = 1
 
-					elif re_separator.search(line):
+					elif "------" in line_val:
 						flag_read[1] = 2
 
 					elif flag_read[1] == 2:
-						if len(line.strip()) == 0:
+						if len(line_val.strip()) == 0:
 							flag_read = [0,0]
 							continue
 
-						i = int(line[8:13].strip()) - 1
-						j = int(line[13:18].strip()) - 1
+						i = int(line_val[8:13].strip()) - 1
+						j = int(line_val[13:18].strip()) - 1
 
-						energies = [float(x.strip()) for x in [line[18:33], line[33:48], line[48:63], line[63:78], line[78:93]]]
-						if line[8:18].strip() in distance_idx:
+						energies = [float(x.strip()) for x in [
+							line_val[18:33],
+							line_val[33:48],
+							line_val[48:63],
+							line_val[63:78],
+							line_val[78:93]
+						]]
+						if line_val[8:18].strip() in distance_idx:
 							energies = [0.0 for x in energies]
 
 						self._energy_ES[i][j] = self._energy_ES[j][i] = energies[0]
 						self._energy_EX[i][j] = self._energy_EX[j][i] = energies[1]
 						self._energy_CT[i][j] = self._energy_CT[j][i] = energies[2]
 						self._energy_DI[i][j] = self._energy_DI[j][i] = energies[3]
-						self._energy_Q[i][j] = float(line[78:93].strip())
-						self._energy_Q[i][j] = -1 * float(line[78:93].strip())
+						self._energy_Q[i][j] = float(line_val[78:93].strip())
+						self._energy_Q[i][j] = -1 * float(line_val[78:93].strip())
 
 				elif flag_read[0] == 4:
 					# 電荷
-					if len(line.strip()) == 0:
+					if len(line_val.strip()) == 0:
 						flag_read = [0,0]
 						continue
 
-					if re_atomic_charge.search(line):
-						atom_idx = int(line[:13].strip())
-						charge = float(line[31:].strip())
+					if RE_ATOMIC_CHARGE.search(line_val):
+						atom_idx = int(line_val[:13].strip())
+						charge = float(line_val[31:].strip())
 						data_idx = [idx for idx, value in enumerate(self._frag_atom) if atom_idx in value][0]
-						self._charge_atom.append([atom_idx, line[14:19].strip(), charge])
+						self._charge_atom.append([atom_idx, line_val[14:19].strip(), charge])
 						self._charge_frag[data_idx] += charge
+		return self
 
-	def get_label(self, frag_idx = None):
-		""" ラベルを返すメソッド """
+
+	def get_label(self, frag_idx=None):
+		"""
+		ラベルを返すメソッド
+
+		Args:
+			frag_idx (int, optional): フラグメントインデックス (Default: None)
+
+		Returns:
+			str: ラベル
+		"""
 		if(frag_idx is None):
 			return self._label
 		else:
 			return self._label[frag_idx - 1]
 
-	def get_fragment_atom(self, frag_idx = None):
-		""" フラグメント構成原子を返すメソッド """
+
+	def get_fragment_atom(self, frag_idx=None):
+		"""
+		フラグメント構成原子を返すメソッド
+
+		Args:
+			frag_idx (int, optional): フラグメントインデックス (Default: None)
+
+		Returns:
+			list: 構成原子のリスト
+		"""
 		if(frag_idx is None):
 			return self._frag_atom
 		else:
 			return self._frag_atom[frag_idx - 1]
 
-	def get_atom_charge(self, atom_idx = None):
-		""" 原子電荷を返すメソッド """
+
+	def get_atom_charge(self, atom_idx=None):
+		"""
+		原子電荷を返すメソッド
+
+		Args:
+			atom_idx (int, optional): 原子インデックス (Default: None)
+
+		Returns:
+			float: 原子電荷
+		"""
 		if atom_idx is None:
 			return self._charge_atom
 		else:
 			return self._charge_atom[atom_idx - 1]
 
-	def get_fragment_charge(self, frag_idx = None):
-		""" フラグメント電荷を返すメソッド """
+
+	def get_fragment_charge(self, frag_idx=None):
+		"""
+		フラグメント電荷を返すメソッド
+
+		Args:
+			frag_idx (int, optional): フラグメントインデックス (Default: None)
+
+		Returns:
+			float: フラグメント電荷
+		"""
 		if frag_idx is None:
 			return self._charge_frag
 		else:
 			return self._charge_frag[frag_idx - 1]
 
-	def get_energy(self, energy_type = "Total", frag_idx = None):
-		""" IFIE エネルギーを返すメソッド """
+
+	def get_energy(self, energy_type="Total", frag_idx=None):
+		"""
+		IFIE エネルギーを返すメソッド
+
+		Args:
+			energy_type (str, optional): `Total`, `ES`, `EX`, `CT`, `DI` or `Q` (Default: "Total")
+			frag_idx (int, optional): フラグメントインデックス (Default: None)
+
+		Returns:
+			list
+		"""
 		energies = None
 		if energy_type == "Total":
-			energies = (self._energy_HF + self._energy_CR) * au
+			energies = (self._energy_HF + self._energy_CR) * AU
 		elif energy_type == "HF":
-			energies = self._energy_HF * au
+			energies = self._energy_HF * AU
 		elif energy_type == "CR":
-			energies = self._energy_CR * au
+			energies = self._energy_CR * AU
 		elif energy_type == "ES":
 			energies = self._energy_ES
 		elif energy_type == "EX":
@@ -200,12 +261,22 @@ class EnergyData:
 			energies = self._energy_Q
 
 		if frag_idx is None:
-			return np.round(energies, digit)
+			return np.round(energies, DIGIT)
 		else:
-			return np.round(energies[frag_idx[0] - 1][frag_idx[1] - 1], digit)
+			return np.round(energies[frag_idx[0] - 1][frag_idx[1] - 1], DIGIT)
 
-	def output_energy(self, energy_type = "Total", output_range = None):
-		""" IFIE エネルギーを出力形式で返すメソッド """
+
+	def output_energy(self, energy_type="Total", output_range=None):
+		"""
+		IFIE エネルギーを出力形式で返すメソッド
+
+		Args:
+			energy_type (str, optional): `Total`, `ES`, `EX`, `CT`, `DI` or `Q` (Default: "Total")
+			output_range (list, optional): 出力するラベルリスト (Default: None)
+
+		Returns:
+			list
+		"""
 		result = None
 
 		if energy_type == "Total":
@@ -238,8 +309,17 @@ class EnergyData:
 		result = [[""] + label] + result
 		return result
 
-	def output_charge(self, output_range = None):
-		""" 電荷情報を出力形式で返すメソッド """
+
+	def output_charge(self, output_range=None):
+		"""
+		電荷情報を出力形式で返すメソッド
+
+		Args:
+			output_range (list, optional): 出力するラベルリスト (Default: None)
+
+		Returns:
+			list
+		"""
 		result = []
 		result_frag = [["Fragment index", "Fragment charge", ""]]
 		result_atom = [["Fragment index", "Atom index", "Atom", "Atomic charge"]]
@@ -249,7 +329,12 @@ class EnergyData:
 			if output_range is None or frag_idx + 1 in output_range:
 				result_frag.append([frag_idx + 1, self._charge_frag[frag_idx], ""])
 				for atom_idx in self._frag_atom[frag_idx]:
-					result_atom.append([frag_idx + 1, self._charge_atom[atom_idx - 1][0], self._charge_atom[atom_idx - 1][1], self._charge_atom[atom_idx - 1][2]])
+					result_atom.append([
+						frag_idx + 1,
+						self._charge_atom[atom_idx - 1][0],
+						self._charge_atom[atom_idx - 1][1],
+						self._charge_atom[atom_idx - 1][2]
+					])
 
 		diff_row = len(result_atom) - len(result_frag)
 		if 0 < diff_row:
