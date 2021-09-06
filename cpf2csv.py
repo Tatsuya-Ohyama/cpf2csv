4bd64a6d98b19fad574414d9c66851c183858e87
Tatsuya Ohyama <t-ohyama@konan-u.ac.jp>
2021-09-06 16:37:47 +0900

    feat: add distance between fragments (`-m` option)
    * change `-pc` to `-p`
    * fix bug for output of HF

--- cpf2csv.py
+++ cpf2csv.py
@@ -16,8 +16,6 @@ from mods.basic_func import *
 from mods.FileLogABINITMP import FileLogABINITMP
 from mods.FileCpf import FileCpf
 
-from pprint import pprint
-
 
 
 # =============== constant =============== #
@@ -29,8 +27,9 @@ OUTPUT_SUFFIX = [
 	"_EX.csv",
 	"_CT.csv",
 	"_DI.csv",
-	"_transq.csv",
-	"_pc.csv"
+	"_q.csv",
+	"_partial_charge.csv",
+	"_min_dist.csv"
 ]
 OUTPUT_NAME = [
 	["Total", "Total energy"],
@@ -41,7 +40,8 @@ OUTPUT_NAME = [
 	["CT", "Charge transfer energy"],
 	["DI", "Dispersion force"],
 	["Q", "Transfer charge"],
-	["PC", "Particle charge"]
+	["P", "Particle charge"],
+	["M", "Minimum distance"]
 ]
 
 
@@ -55,7 +55,7 @@ if __name__ == '__main__':
 	global_option.add_argument("-O", dest="FLAG_OVERWRITE", action="store_true", default=False, help="overwrite forcibly (Default: False)")
 
 	output_type = parser.add_argument_group(title="energy type option", description="energy type for output (default: -t)")
-	output_type.add_argument("-a", "--all", dest="FLAG_ALL", action="store_true", default=False, help="select all type, same as -tfesxcdq")
+	output_type.add_argument("-a", "--all", dest="FLAG_ALL", action="store_true", default=False, help="select all type, same as -tfesxcdqm")
 	output_type.add_argument("-t", "--total", dest="FLAG_TOTAL", action="store_true", default=False, help="total energy (kcal/mol)")
 	output_type.add_argument("-f", "--hartree", dest="FLAG_HF", action="store_true", default=False, help="Hartree-Fock energy (kcal/mol)")
 	output_type.add_argument("-e", "--correlation", dest="FLAG_CORR", action="store_true", default=False, help="electron correlation energy (kcal/mol)")
@@ -64,7 +64,8 @@ if __name__ == '__main__':
 	output_type.add_argument("-c", "--chargetransfer-mix", dest="FLAG_CT", action="store_true", default=False, help="charge transfer and other interaction energy (CT+mix) (kcal/mol)")
 	output_type.add_argument("-d", "--dispersion", dest="FLAG_DI", action="store_true", default=False, help="dispersion energy (DI) (kcal/mol)")
 	output_type.add_argument("-q", "--chargetransfer-amount", dest="FLAG_Q", action="store_true", default=False, help="amount of charge transfer (e; I(row) -> J(col))")
-	output_type.add_argument("-pc", "--partial-charge", dest="FLAG_PC", action="store_true", default=False, help="partial charge")
+	output_type.add_argument("-p", "--partial-charge", dest="FLAG_PC", action="store_true", default=False, help="partial charge")
+	output_type.add_argument("-m", "--min-dist", dest="FLAG_MIN_DIST", action="store_true", default=False, help="minimum distance")
 
 	output_range = parser.add_mutually_exclusive_group()
 	output_range.add_argument("--include", dest="INCLUDE", metavar="Frag_No.", nargs="+", help="")
@@ -83,7 +84,8 @@ if __name__ == '__main__':
 		args.FLAG_CT,
 		args.FLAG_DI,
 		args.FLAG_Q,
-		args.FLAG_PC
+		args.FLAG_PC,
+		args.FLAG_MIN_DIST
 	]
 
 	if args.FLAG_ALL:
@@ -126,13 +128,16 @@ if __name__ == '__main__':
 			if args.FLAG_OVERWRITE == False:
 				check_overwrite(output)
 
-			if OUTPUT_NAME[idx][0] == "PC":
-				with open(output, "w") as obj_output:
-					csv_writer = csv.writer(obj_output, lineterminator="\n")
+			with open(output, "w") as obj_output:
+				csv_writer = csv.writer(obj_output, lineterminator="\n")
+				if OUTPUT_NAME[idx][0] == "P":
 					csv_writer.writerows(data_FMO.output_charge(output_range))
-					sys.stderr.write("create: %s (partial charge)\n" % output)
-			else:
-				with open(output, "w") as obj_output:
-					csv_writer = csv.writer(obj_output, lineterminator="\n")
+					sys.stderr.write("create: {0} (partial charge)\n".format(output))
+
+				elif OUTPUT_NAME[idx][0] == "M":
+					csv_writer.writerows(data_FMO.output_min_dist(output_range))
+					sys.stderr.write("create: {0} (minimum distance)\n".format(output))
+
+				else:
 					csv_writer.writerows(data_FMO.output_energy(OUTPUT_NAME[idx][0], output_range))
 					sys.stderr.write("create: {0} ({1})\n".format(output, OUTPUT_NAME[idx][1]))
